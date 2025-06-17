VoltageOS/hardware_voltage_interfaces

interfaces: cleanup
Change-Id: I108efa15757d0372f4fdd63a12aabc3cd34a8189
Signed-off-by: Dmitrii <bankersenator@gmail.com>

update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.voltage:hardware/voltage/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"

CherishOS/android_hardware_cherish_interfaces

Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
Signed-off-by: Hung Phan <phandinhhungvp2001@gmail.com>

update-makefiles.sh
@@ -3,7 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.cherish:hardware/cherish/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
 